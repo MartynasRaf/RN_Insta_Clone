a97c18e202243b9ca79a7fb80a97d84bad69fda9
Martynas R <m.rafanavicius@gmail.com>
2020-06-06 16:46:03 +0300

    Styling

--- src/Screens/LoginScreen.js
+++ src/Screens/LoginScreen.js
@@ -6,27 +6,45 @@ import {
   StyleSheet,
   Image,
   Dimensions,
+  Text,
 } from 'react-native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faUser, faLock} from '@fortawesome/free-solid-svg-icons';
+
 const LoginScreen = () => {
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   return (
     <View style={styles.container}>
-      <Image source={require('../../assets/Logo.png')} style={styles.image} />
-      <TextInput
-        style={styles.textInput}
-        onChangeText={(text) => setUserName(text)}
-        value={userName}
-        placeholder="User Name"
-      />
-      <TextInput
-        style={styles.textInput}
-        onChangeText={(text) => setPassword(text)}
-        value={password}
-        placeholder="Password"
-        secureTextEntry={true}
-      />
-      <TouchableOpacity style={styles.button}></TouchableOpacity>
+      <View>
+        <Text style={styles.loginText}>Login</Text>
+        <Text style={styles.hintText}>Username</Text>
+        <View style={styles.inputContainer}>
+          <FontAwesomeIcon icon={faUser} />
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(text) => setUserName(text)}
+            value={userName}
+            placeholder="Type your username"
+          />
+        </View>
+        <Text style={styles.hintText}>Password</Text>
+        <View style={styles.inputContainer}>
+          <FontAwesomeIcon icon={faLock} />
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+            placeholder="Type your password"
+          />
+        </View>
+        <TouchableOpacity style={styles.forgotContent}>
+          <Text>Forgot Password?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -37,27 +55,50 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  image: {
-    width: 100,
-    height: Dimensions.get('window').height / 4,
-    resizeMode: 'stretch',
-  },
-  textInput: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    width: Dimensions.get('window').width / 1.5,
-    borderRadius: 50,
-    paddingHorizontal: 10,
-    marginVertical: 10,
-  },
   button: {
     alignItems: 'center',
-    backgroundColor: '#11D111',
-    padding: 10,
+    backgroundColor: '#4bc8db',
+    height: 40,
+    width: Dimensions.get('window').width / 1.5,
+    borderRadius: 50,
+    marginVertical: 10,
+    justifyContent: 'center',
+  },
+  text: {
+    fontWeight: 'bold',
+  },
+  hintText: {
+    justifyContent: 'flex-start',
+    marginBottom: -5,
+    marginTop: 5,
+    fontWeight: 'bold',
+  },
+  forgotContent: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+  loginText: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    alignSelf: 'center',
+    marginBottom: 40,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    borderColor: '#888',
+    borderBottomWidth: 1,
+    width: Dimensions.get('window').width / 1.5,
+  },
+  textInput: {
+    borderWidth: 0,
+    paddingHorizontal: 10,
   },
 });
 
 export default LoginScreen;
 
 //      <CacheImage uri="assets\bg.jpg" style={styles.width} />
+// /<Image source={require('../../assets/Logo.png')} style={styles.image} />
